563c0c131863d5fd3c057340496f9e145e857f35
ArturZhelnov <94010703+ArturZhelnov@users.noreply.github.com>
2021-12-24 18:36:37 +0200

    Compressed some files

--- portfolio/js/new/main.js
+++ portfolio/js/new/main.js
@@ -26,13 +26,13 @@ $(function(){
 		closeOnContentClick: true,
 		closeBtnInside: false,
 		fixedContentPos: true,
-		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		mainClass: 'mfp-no-margins mfp-with-zoom',/* class to remove default margin from left and right side*/
 		image: {
 			verticalFit: true
 		},
 		zoom: {
 			enabled: true,
-			duration: 300 // don't foget to change the duration also in CSS
+			duration: 300/* don't foget to change the duration also in CSS*/
 		}
 	});
 	/* MAGNIFIC POPUP ENDS */
@@ -82,7 +82,5 @@ $(function(){
             }), 1200;
         }
     });
-	
-	
-	
+
 });
